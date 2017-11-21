966e9917c1749affc2ee993beaa91fb5a8085eb3
Andrew Lockhart <lockan@gmail.com>
2017-11-21 20:24:50 +1300

    Lecture 66 - model validation and fetching

--- todo/playground/sqlite-basic-db.js
+++ todo/playground/sqlite-basic-db.js
@@ -1,32 +1,80 @@
 var Sequelize = require('sequelize');
 var sqlz = new Sequelize(
-	undefined, 
-	undefined, 
-	undefined, 
-	{ 
-		'dialect' : 'sqlite', 
-		'storage' : __dirname + '\\sqlite-basic-db.sqlite', 
-		'operatorsAliases' : false
+	undefined,
+	undefined,
+	undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '\\sqlite-basic-db.sqlite',
+		'operatorsAliases': false
 	}
 );
 
-var ToDo = sqlz.define('todo', 
-	{ 
-		description: { type: Sequelize.STRING }, 
-		completed: { type: Sequelize.BOOLEAN }
-	}
-);
+const Ops = Sequelize.Op;
 
-sqlz.sync({force:true}).then(function() { 
+var ToDo = sqlz.define('todo', {
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false, 
+		validate: { 
+			len: [1, 250]
+		}
+	},
+	completed: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+	}
+});
+
+sqlz.sync({
+	// force: true
+}).then(function() {
 	console.log("Sync completed");
 
-	ToDo.create(
-		{
-			description: "learn sqlite and sequelize",
-			completed: false
+	return ToDo.findOne( {
+		where: {
+			description: {
+				[Ops.like]: "%learn%"
+			}
 		}
-	).then(function(todo) {
-		console.log('Finished');
-		console.log(todo);
+	})
+	.then( function(todo) {
+		if (todo) {
+			console.log(todo.toJSON()); 
+		} else {
+			console.log("Todo not found");
+		}
+	})
+	.catch(function(e){
+		console.log(e);
 	});
+
+	// ToDo.create({
+	// 	description: "learn sqlite and sequelize",
+	// 	completed: false
+	// })
+	// .then(function(todo) {
+	// 	return ToDo.create({
+	// 		description: "Add database to To-Do Application"
+	// 	});
+	// })
+	// .then(function() {
+	// 	return ToDo.findAll({
+	// 		where: {
+	// 			completed: false
+	// 		}
+	// 	})
+	// })
+	// .then(function(todos) {
+	// 	if (todos) { 
+	// 		todos.forEach( function(todo) {
+	// 			console.log(todo.toJSON());	
+	// 		});
+	// 	} else {
+	// 		console.log("No todo found");
+	// 	}
+	// })
+	// .catch(function(e) {
+	// 	console.log(e);
+	// });
 });
