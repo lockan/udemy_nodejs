29363701650ece4feba5aad5e85032a220c17b4c
lockan <lockan@gmail.com>
2018-04-30 20:41:04 +1200

    postgres <-> sqlite environment switch

--- todo/db.js
+++ todo/db.js
@@ -1,13 +1,22 @@
+var env = process.env.NODE_ENV || 'dev';
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(
-	undefined,
-	undefined,
-	undefined, {
-		'dialect': 'sqlite',
-		'storage': __dirname + '\\data\\dev-todo-api.sqlite',
-		'operatorsAliases': false
-	}
-);
+var sequelize;
+
+if (env === 'production') { 
+	sequelize = new Sequelize(process.env.DATABASE_URL, { 
+		'dialect': 'postgres',
+	});
+} else { 
+	sequelize = new Sequelize(
+		undefined,
+		undefined,
+		undefined, {
+			'dialect': 'sqlite',
+			'storage': __dirname + '\\data\\dev-todo-api.sqlite',
+			'operatorsAliases': false
+		}
+	);
+}
 
 const Ops = Sequelize.Op;
 
