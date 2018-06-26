035fddfc5587b89ae2530aee29dc6e12a996052f
lockan <lockan@gmail.com>
2018-06-26 22:26:48 +1200

    Stubbed in delete, but server needs updating for put and delete

--- todo/tests/server.test.js
+++ todo/tests/server.test.js
@@ -7,49 +7,49 @@ beforeAll( () => {
   console.log("Running tests...");
 });
 
-// // GET /todos
-// test("GET /todos", () => {
-// 	return doGet(testUrl)
-// 	.then( function (response) {
-// 		expect(queryHasItems(response)).toBe(true);
-// 	});
-// });
+// GET /todos
+test("GET /todos", () => {
+	return doGet(testUrl)
+	.then( function (response) {
+		expect(queryHasItems(response)).toBe(true);
+	});
+});
 
-// // GET /todos?complete=true
-// test("GET /todos?complete=true", () => { 
-// 	return doGet(testUrl + "?completed=true")
-// 	.then( function (response) {
-// 		expect(queryHasItems(response)).toBe(false);
-// 	});
-// });
+// GET /todos?complete=true
+test("GET /todos?complete=true", () => { 
+	return doGet(testUrl + "?completed=true")
+	.then( function (response) {
+		expect(queryHasItems(response)).toBe(false);
+	});
+});
 
-// // GET /todos?complete=false
-// test("GET /todos?complete=false", () => { 
-// 	return doGet(testUrl + "?completed=false")
-// 	.then( function (response) {
-// 		expect(queryHasItems(response)).toBe(true);
-// 	});
-// });
+// GET /todos?complete=false
+test("GET /todos?complete=false", () => { 
+	return doGet(testUrl + "?completed=false")
+	.then( function (response) {
+		expect(queryHasItems(response)).toBe(true);
+	});
+});
 
-// // GET /todos/:id
-// test("GET /todos?id=1", () => { 
-// 	return doGet(testUrl + "?id=1")
-// 	.then( function (response) {
-// 		expect(queryHasItems(response)).toBe(true);
-// 	});
-// });
+// GET /todos/:id
+test("GET /todos?id=1", () => { 
+	return doGet(testUrl + "?id=1")
+	.then( function (response) {
+		expect(queryHasItems(response)).toBe(true);
+	});
+});
 
-// // POST /todos
-// test("POST /todos", () => {
-// 	var todo = { 
-// 		"description": "New Item added by unit test",
-// 		"completed": false
-// 	};
-// 	return doPost(testUrl, todo)
-// 	.then( function (response) {
-// 		expect(queryHasItems(response)).toBe(true);
-// 	});
-// });
+// POST /todos
+test("POST /todos", () => {
+	var todo = { 
+		"description": "New Item added by unit test",
+		"completed": false
+	};
+	return doPost(testUrl, todo)
+	.then( function (response) {
+		expect(queryHasItems(response)).toBe(true);
+	});
+});
 
 // PUT /todos/:id
 test("PUT /todos/:id", () => {
@@ -71,6 +71,20 @@ test("PUT /todos/:id", () => {
 });
 
 // DELETE /todos/:id
+test("DELETE /todos/:id", () => {
+	//doGet( complete)....
+	return doGet(testUrl + "?completed=true")
+	.then( (todolist) => {
+		// get id of first complete
+		var todos = JSON.parse(todolist);
+		var t_id = todos[0].id;
+		return doDelete(testUrl + "/" + t_id)
+		.then ( (response) => { 
+			//TODO: fix response for DELETE
+			expect(todoIsComplete(response)).toBe(true);
+		});
+	});
+});
 
 afterAll( () => {
 	console.log("Tests complete");
@@ -127,6 +141,28 @@ function doPut(url, payload) {
 				console.log(err);
 				reject(err);
 			};
+			if(data !== undefined && resp.statusCode == 200) { 
+				resolve(data);	
+			} else { 
+				reject(undefined);
+			}
+		});
+	});
+}
+
+function doDelete(url) { 
+	return new Promise(function (resolve, reject) { 
+		var options = { 
+			url: url,
+			json: null, 
+		}
+
+		request.delete(options, (err, resp, data) => {
+			if (err) { 
+				console.log(err);
+				reject(err);
+			};
+			//TODO: test and fix if needed
 			if(data !== undefined && resp.statusCode == 200) { 
 				resolve(data);	
 			} else { 
